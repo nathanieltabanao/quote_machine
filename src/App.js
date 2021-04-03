5fed99b1e91c5ec7a82f1afc880dc848dd04c5ed
Nathaniel Angelico Tabanao <nathanielangelico23@gmail.com>
2021-04-03 15:48:18 +0800

    Working Version 1 Quote Machine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {React, Component} from 'react';
-import QuoteBox from './quote-box';
+//import QuoteBox from './quote-box';
 import quoteData  from './quotes.json';
 
 const quotes = quoteData.quotes;
@@ -9,37 +9,56 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      id: 0,
+      id: this.getRandomInt(),
       quote: '',
       author: '',
     };
   }
 
-  getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+  getRandomInt = () => {
+    const min = Math.ceil(1);
+    const max = Math.floor(1639);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  getQuote = (id) => {
-     return quotes.filter(q => (
-      q.id === id
+  updateQuote = () => {
+    let q = quotes
+    .filter(q => (
+      q.id===this.getRandomInt()
     ))
-    .map(q => ({
-      id: q.id,
-      quote: q.quote,
-      author: q.author
+    .map(qu => ({
+      id: qu.id,
+      quote: qu.quote,
+      author: qu.author,
     }))
-    
+    console.log('value of q: ',q);
+    console.log('before setState state: ',this.state);
+    this.setState({
+      author: q[0].author,
+      quote: q[0].quote,
+      id: q[0].id,
+    })
+    console.log('updated state: ',this.state);
+    console.log('value of q after state update: ',q);
+    console.log('value of q id after state update: ',q[0]);
+  }
+
+  tweetQuote = () => {
+
   }
 
   render(){
-      console.log("test: " + this.getQuote(1));
-      console.log(quotes.length);
       return (
         <div className="App">
           <div className="Main grid-container">
-            <QuoteBox {...this.state} newQuote={this.getQuote}/>
+          <div id="quote-box">
+                  <p id='text' key={this.id}>"{this.state.quote}"</p>
+                  <p id='author'>- {this.state.author}</p>
+                  <div className="row">
+                    <button id='new-quote' className="btn" onClick={this.updateQuote}>New Quote</button>
+                    <a id='tweet-quote' href='twitter.com/intent/tweet' target='_blank'><button className="btn">Tweet Button</button></a>
+                  </div>
+            </div>
           </div>
         </div>
       );
--- src/quote-box.js
+++ src/quote-box.js
@@ -1,15 +1,8 @@
 function QuoteBox (props){
-    const {quote, author, newQuote} = props;
-    return(
-        <div id="quote-box">
-                  <p id='text'>"{quote}"</p>
-                  <p id='author'>- {author}</p>
-                  <div className="row">
-                    <button id='new-quote' className="btn" onClick={() =>newQuote(2)}>New Quote</button>
-                    <a id='tweet-quote' href='null'><button className="btn">Tweet Button</button></a>
-                  </div>
-            </div>
-    )
+    // const {id, quote, author, newQuote} = props;
+    // console.log("id: ",id);
+    // console.log('quote: ',quote);
+    // console.log('props: ',props);
 }
 
 export default QuoteBox;
